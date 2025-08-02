d03b532d33a8ea523669c5c5fba34774f2a9b032
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2025-08-02 19:18:59 -0300

    feat: adicionado storage

--- src/application/modules/home/index.tsx
+++ src/application/modules/home/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { useStyles } from 'react-native-unistyles';
-import { stylesheet } from './styles';
-import { Text } from 'react-native';
-import { Layout } from '@/application/components/layout';
+import React from "react";
+import { useStyles } from "react-native-unistyles";
+import { stylesheet } from "./styles";
+import { Layout } from "@/application/components/layout";
+import { Text } from "@/application/components/base/text";
 
 export const Home = () => {
   const { styles } = useStyles(stylesheet);
 
   return (
     <Layout style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      <Text size={50} weight="bold">
+        Home
+      </Text>
     </Layout>
   );
 };
