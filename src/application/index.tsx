497816fc9f7b0c77e2e3dcdc7bae783345a8f392
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2025-08-03 14:49:34 -0300

    refactor: adicionado estilo ao SafeAreaProvider

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,10 +1,15 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Home } from "./modules/home";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ToastManager from "toastify-react-native";
 
 export const App = () => {
   return (
-    <SafeAreaProvider>
-      <Home />
+    <SafeAreaProvider style={{flex: 1}}>
+      <GestureHandlerRootView>
+        <Home />
+        <ToastManager />
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 };
