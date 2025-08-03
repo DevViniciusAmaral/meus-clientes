f8cba5761279c4a8d0ac086db7014ce594905f5f
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2025-08-03 10:09:12 -0300

    refactor: adicionado propriedade contentContainerStyle

--- src/application/components/searchbar/index.tsx
+++ src/application/components/searchbar/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useStyles } from "react-native-unistyles";
 import { stylesheet } from "./styles";
-import { View } from "react-native";
+import { StyleProp, View, ViewStyle } from "react-native";
 import { TextInput, TextInputProps } from "../base/text-input";
 import { Search } from "lucide-react-native";
 
-export const Searchbar = ({ ...rest }: TextInputProps) => {
+interface SearchbarProps extends TextInputProps {
+  contentContainerStyle?: StyleProp<ViewStyle>;
+}
+
+export const Searchbar = ({
+  contentContainerStyle,
+  ...rest
+}: SearchbarProps) => {
   const { styles, theme } = useStyles(stylesheet);
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, contentContainerStyle]}>
       <TextInput {...rest} />
       <Search size={20} color={`${theme.colors.typography}40`} />
     </View>
